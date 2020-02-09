379e1bc8061e40fc02da7a269aa2879341e57e74
lazward <lazwerd@gmail.com>
2020-02-09 18:05:58 -0500

    Slingshot now functional

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -14,6 +14,8 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile ;
     public bool aimingMode ;
 
+    public float velocityMult = 4f ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,38 @@ public class Slingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (!aimingMode) {
+
+            return ;
+
+        }
+
+        Vector3 mousePos2D = Input.mousePosition ;
+        mousePos2D.z = -Camera.main.transform.position.z ;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D) ;
+        Vector3 mouseDelta = mousePos3D-launchPos ;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius ;
+
+        if (mouseDelta.magnitude > maxMagnitude) {
+
+            mouseDelta.Normalize() ;
+            mouseDelta *= maxMagnitude ;
+
+        }
+
+        Vector3 projPos = launchPos + mouseDelta ;
+        projectile.transform.position = projPos ;
+
+        if (Input.GetMouseButtonUp(0)) {
+
+            aimingMode = false ;
+            projectile.GetComponent<Rigidbody>().isKinematic = false ;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult ;
+            projectile = null ;
+
+        }
+
         
     }
 
